4d4ae40bc9de6ac60b8428c2f77c92bf16a3b328
prateeksha9 <66215313+prateeksha9@users.noreply.github.com>
2021-10-19 16:13:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
 const path = require('path'); 
-const port = process.env.PORT || 80 
+const port = process.env.PORT || 5000;
 const db = require('./config/mongoose');
 const app = express(); // storing express
 
